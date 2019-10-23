0e04ea4b02e25afbdaebae783a0fefee43e3007d
TheEdward162 <theedward162@gmail.com>
2019-10-23 22:40:58 +0200

    cli args

--- src/input_source.rs
+++ src/input_source.rs
@@ -7,10 +7,25 @@ pub fn list_fitting_input_devices(host: &Host, channels: u16, sample_rate: u32, 
 	let mut result = Vec::new();
 
 	log::info!("Host {:?} devices:", host.id());
-	for (i, device) in host.devices().expect("Could not read devices").enumerate() {
+	let mut devices = match host.devices() {
+		Err(e) => {
+			log::warn!("Could not read devices: {:?}", e);
+			return result
+		}
+		Ok(d) => d
+	};
+
+	for (i, device) in devices.enumerate() {
 		log::info!("\t{}) {:?}:", i, device.name().expect("Could not retrieve device name"));
 
-		for format in device.supported_input_formats().expect("Could not read device input formats") {
+		let mut formats = match device.supported_input_formats() {
+			Err(e) => {
+				log::warn!("Could not read formats: {:?}", e);
+				return result
+			}
+			Ok(f) => f
+		};
+		for format in formats {
 			log::info!("\t\t{}ch ({} - {}) {:?}", format.channels, format.min_sample_rate.0, format.max_sample_rate.0, format.data_type);
 			if 
 				format.channels == channels
--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,11 @@ const BINS: usize = WINDOW_SIZE / BIN_SIZE;
 /// Number of spectrum bins.
 const SPECTRUM_BINS: usize = BINS / 2 - 1;
 
-// TODO: Read from cli.
-const SERIAL_PORT: &'static str = "/dev/tty.usbserial-14110";
 
-fn prepare_input_stream() -> (cpal::Host, cpal::EventLoop, cpal::StreamId) {
+const DEFAULT_SERIAL_PORT: &'static str = "/dev/ttyUSB0";
+const DEFAULT_DEVICE_INDEX: usize = 0;
+
+fn prepare_input_stream(mut device_index: usize) -> (cpal::Host, cpal::EventLoop, cpal::StreamId) {
 	let host = cpal::default_host();
 	let event_loop = host.event_loop();
 
@@ -43,7 +44,17 @@ fn prepare_input_stream() -> (cpal::Host, cpal::EventLoop, cpal::StreamId) {
 			panic!("Could not find any fitting device.");
 		}
 
-		let device = host.devices().unwrap().nth(fitting_device_indices[0]).unwrap();
+		log::debug!("Fitting device indices:");
+		for index in fitting_device_indices.iter() {
+			log::debug!("\t{}", index);
+		}
+
+		if device_index >= fitting_device_indices.len() {
+			log::warn!("Index {} is bigger than number of fitting devices ({}). Using 0.", device_index, fitting_device_indices.len());
+			device_index = 0;
+		}
+
+		let device = host.devices().unwrap().nth(fitting_device_indices[device_index]).unwrap();
 		let format = cpal::Format {
 			channels: CHANNELS as u16,
 			sample_rate: cpal::SampleRate(SAMPLE_RATE),
@@ -61,17 +72,32 @@ fn prepare_input_stream() -> (cpal::Host, cpal::EventLoop, cpal::StreamId) {
 }
 
 fn main() {
-	// edwardium_logger::init(
-	// 	vec![
-	// 		edwardium_logger::StdoutTarget::new(
-	// 			log::Level::Trace, Default::default()
-	// 		)
-	// 	]
-	// ).expect("Could not initialize logger");
+	edwardium_logger::init(
+		vec![
+			edwardium_logger::StdoutTarget::new(
+				log::Level::Trace, Default::default()
+			)
+		]
+	).expect("Could not initialize logger");
 
-	let (_host, event_loop, input_stream_id) = prepare_input_stream();
+	let args: Vec<String> = std::env::args().collect();
+	
+	let serial;
+	let index: usize;
+	if args.len() <= 1 {
+		serial = DEFAULT_SERIAL_PORT;
+		index = DEFAULT_DEVICE_INDEX;
+	} else if args.len() <= 2 {
+		serial = &args[1];
+		index = DEFAULT_DEVICE_INDEX;
+	} else {
+		serial = &args[1];
+		index = args[2].parse::<usize>().unwrap_or(DEFAULT_DEVICE_INDEX);
+	}
 
-	let mut context = context::Context::new(SERIAL_PORT);
+	let (_host, event_loop, input_stream_id) = prepare_input_stream(index);
+
+	let mut context = context::Context::new(serial);
 
 	event_loop.run(move |id, result| {
 		let data = match result {
